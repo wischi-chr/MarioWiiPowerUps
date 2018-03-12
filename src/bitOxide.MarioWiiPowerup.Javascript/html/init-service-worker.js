5a652cf1a84f3809b1686d88ac5175cccd927876
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-03-12 19:18:35 +0100

    Fix service-worker register url

--- src/bitOxide.MarioWiiPowerup.Javascript/html/init-service-worker.js
+++ src/bitOxide.MarioWiiPowerup.Javascript/html/init-service-worker.js
@@ -1,7 +1,7 @@
 function init_serviceworker()
 {
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/service-worker.js',{
+		navigator.serviceWorker.register('./service-worker.js',{
 			scope: '.'
 		});
 	}
